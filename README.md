853bfab3ac71b6e170a5c8b0bef628bae462bff2
LundenFood2025 <hillar.valgma@conviso.ee>
2025-10-02 17:37:44 +0300

    upload

--- README.md
+++
@@ -1,1 +0,0 @@
-# LundenFood-360
---
+++ data.js
@@ -0,0 +1,155 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1-f",
+      "name": "1 f",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": -2.743604334412673,
+        "pitch": 0.5897047226511738,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.1354813053827613,
+          "pitch": 0.4400775370292891,
+          "rotation": 18.84955592153877,
+          "target": "1-2-f"
+        },
+        {
+          "yaw": -2.1693146484295873,
+          "pitch": 0.3644364841802723,
+          "rotation": 0.7853981633974483,
+          "target": "2-3-f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2-f",
+      "name": "2 f",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.06411413578755543,
+        "pitch": 0.6081832458881209,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5804360818280365,
+          "pitch": 0.39153486853360064,
+          "rotation": 0,
+          "target": "0-1-f"
+        },
+        {
+          "yaw": -0.7525237844416548,
+          "pitch": 0.403640074994545,
+          "rotation": 0,
+          "target": "2-3-f"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3-f",
+      "name": "3 f",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.27070412888071615,
+        "pitch": 0.6154462151066902,
+        "fov": 1.4324874404875778
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.038480644279863085,
+          "pitch": 0.41582895738071457,
+          "rotation": 0,
+          "target": "0-1-f"
+        },
+        {
+          "yaw": 0.9205902784514954,
+          "pitch": 0.4671304394611493,
+          "rotation": 0.7853981633974483,
+          "target": "1-2-f"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "LundenFood-360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
